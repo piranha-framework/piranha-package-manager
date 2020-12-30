f39b74c1537c09f8e7b1faafc8a72ce45736c798
Masud karim <unroot@localhost.localdomain>
2020-12-30 12:07:03 -0900

    cloning machanism

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from src import git
+
+def main():
+    git.clone_repo()
+
+if __name__ == "__main__":
+    main()
---
+++ src/git.py
@@ -0,0 +1,6 @@
+import os
+
+def clone_repo():
+    url = "https://github.com/Masud2017/piranha-framework.git"
+    repo_dir = "/home/unroot/Desktop"
+    os.system("git clone "+url)
