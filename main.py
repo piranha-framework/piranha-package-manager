9626536d6324b1a7f3b943a86495037ab7b7f9c2
Masud karim <msmasud578@gmail.com>
2021-08-21 20:05:09 -0800

    Updating lilbit

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
 from src import git
+import optparse
 
 def main():
-    git.clone_repo()
+    flag = optparse.OptionParser()
+    flag.add_option('--person', '-p',default="world")
+    flag.add_option('--makemigrations','-M') # This will generate the migration informations 
+    flag.add_option('--migrate', '-m') # This will do the actual migration in database
+
+    options,arguments = flag.parse_args()
+    print("Hello %s"% options.person)
+    print("Hello %s"%arguments)
+#    git.clone_repo()
 
 if __name__ == "__main__":
     main()
